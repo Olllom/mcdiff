66a5cd859e8f7720a268fb802bc564dd49d382b4
Andreas Krämer <kraemer.research@gmail.com>
2018-05-18 12:17:59 -0400

    Setup command installs requirements automatically

--- setup.py
+++ setup.py
@@ -1,29 +1,38 @@
 #!/bin/env/python
 
-
-if __name__ == "__main__":
+try:
+    from setuptools import setup
+except ImportError:
     from distutils.core import setup
-    from glob import glob
 
-    setup(name='mcdiff',
-        version='0.001',
-        description='Program to find diffusion parameters with Monte Carlo routine.',
-        #author='An Ghysels',
-        #author_email='An.Ghysels@UGent.be',
-        #url='http://molmod.ugent.be/code/',
-        package_dir = {'mcdiff': 'lib'},
-        packages = ['mcdiff','mcdiff.tools','mcdiff.permeability'],
-        scripts=glob("scripts/*"),
+from glob import glob
 
 
-        classifiers=[
-            #'Development Status :: 3 - Alpha',
-            #'Environment :: Console',
-            #'Intended Audience :: Science/Research',
-            #'License :: OSI Approved :: GNU General Public License (GPL)',
-            #'Operating System :: POSIX :: Linux',
-            'Programming Language :: Python :: 2.7'
-        ],
+setup(name='mcdiff',
+    version='0.001',
+    description='Program to find diffusion parameters with Monte Carlo routine.',
+    #author='An Ghysels',
+    #author_email='An.Ghysels@UGent.be',
+    #url='http://molmod.ugent.be/code/',
+    package_dir = {'mcdiff': 'lib'},
+    packages = ['mcdiff','mcdiff.tools','mcdiff.permeability'],
+    scripts=glob("scripts/*"),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "six"
+    ],
 
-    )
+
+    classifiers=[
+        #'Development Status :: 3 - Alpha',
+        #'Environment :: Console',
+        #'Intended Audience :: Science/Research',
+        #'License :: OSI Approved :: GNU General Public License (GPL)',
+        #'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7'
+    ],
+
+)
 
